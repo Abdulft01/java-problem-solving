e8dcb74b06a4045bcda7efcdb275d880c59f5f5c
Abdulft01 <abdulraheemft01@gmail.com>
2026-04-16 22:14:46 +0530

    to find factorial of a number without using  recursion

---
+++ number-system/factorialNoRec.java
@@ -0,0 +1,14 @@
+public class factorialNoRec {
+    public static int factorial(int n){
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println("Factorial of "+n+" is: "+factorial(n));
+    }
+}
