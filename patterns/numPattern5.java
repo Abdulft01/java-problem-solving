b917c1b9bfecaea966fb7eae51aeaa25ff9e156e
Abdulft01 <abdulraheemft01@gmail.com>
2026-04-25 22:19:48 +0530

    number pattern 5

---
+++ patterns/numPattern5.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class numPattern5 {
+    public static void main(String[] args) {
+        int n=5;
+        int x = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.printf("%4d",x);
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
